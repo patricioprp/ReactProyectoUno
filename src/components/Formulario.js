7792875c847a263fa924b74b30971f7d49e4163c
luca.tensolite <lucaf@tensolite.com>
2021-02-22 17:54:08 -0300

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from "react";
+
+const Formulario = () => {
+    return(
+        <Fragment>
+            <h2>Crear cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                <input />
+            </form>
+        </Fragment>
+    );
+}
+
+export default Formulario;
